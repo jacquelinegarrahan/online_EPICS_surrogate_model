bc1d69de508d2c87a009b7daa2e3897013d7b6fc
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-08 18:34:14 -0400

    WIP: Set up put handlers for pva server

--- scalar_demo/pva_example/pva_online_surrogate_model.py
+++ scalar_demo/pva_example/pva_online_surrogate_model.py
@@ -10,28 +10,65 @@ import random
 from p4p.nt import NTScalar
 from p4p.server.thread import SharedPV
 from p4p.server import Server
+from p4p.client.thread import Context
+from p4p.rpc import rpc, quickRPCServer
 
 
-class PVServer:
-    def __init__(self, pvdb):
+CONTEXT = Context("pva")
 
-        self.pvdb = pvdb
+class PVAServer:
+    def __init__(self, in_pvdb, out_pvdb, model):
+
+        self.in_pvdb = in_pvdb
+        self.out_pvdb = out_pvdb
         self.providers = {}
         self.input_pv_state = {}
+        self.output_pv_state = {}
 
-        for pvname in pvdb.keys():
+        # create PVs for model inputs
+        for pvname in in_pvdb:
 
-            self.input_pv_state = pvdb[pvname]["value"]
+            self.input_pv_state = in_pvdb[pvname]["value"]
 
-            pv = SharedPV(nt=NTScalar("d"), initial=pvdb[pvname]["value"])
+            pv = SharedPV(nt=NTScalar("d"), initial=in_pvdb[pvname]["value"])
 
+            # put handlers receive callbacks with put operation requested
             @pv.put
             def onPut(pv, op):
                 pv.post(op.value())
+                # run model
+                print("Updated value: %s - %s", op.name(), op.value())
+                self.output_pv_state = self.model.run(self.input_pv_state, verbose=True)
+                # do we want this to be blocking?? 
+                self.set_output_pvs(output_pv_state)
                 op.done()
 
             self.providers[pvname] = pv
 
-    def start_server(self):
+        # create PVs for model outputs
+        for pvname in out_pvdb:
+            self.input_pv_state = out_pvdb[pvname]["value"]
 
+            pv = SharedPV(nt=NTScalar("d"), initial=out_pvdb[pvname]["value"])
+
+            # put handlers receive callbacks with put operation requested
+            @pv.put
+            def onPut(pv, op):
+                pv.post(op.value())
+                # run model
+                print("Ouput value: %s - %s", op.name(), op.value())
+                op.done()
+
+            self.providers[pvname] = pv
+
+
+    def set_output_pvs(self, output_pvs):
+        for pvame, value in self.output_pv_state:
+            providers[pvname].put(value)
+
+    def start_server(self):
+        # no need to poll
+        prefix = "test_prefix"
         Server.forever(providers=[self.providers])
+
+
