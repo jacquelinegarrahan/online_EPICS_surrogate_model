b12a796674c51938598c8fdb5a507002cafeb47d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-11 14:51:50 -0400

    MNT: Set up server with single command and protocol arg'

--- bin/commands/serve.py
+++ bin/commands/serve.py
@@ -9,13 +9,20 @@ def serve():
     pass
 
 
-@serve.command()
-def launch_ca_server():
-    from scalar_demo.model.surrogate_model import SurrogateModel
-    from scalar_demo.ca_example.online_surrogate_model import SyncedSimPVServer
 
+@serve.command()
+@click.argument('protocol')
+def start_server(protocol):
+    """
+    Start server using given PROTOCOL.
+
+    PROTOCOL options are 'ca' and 'pva'
+    """
+
+    from scalar_demo.model.surrogate_model import SurrogateModel
     sm = SurrogateModel(model_file=MODEL_FILE)
 
+    # set up process variable databases
     cmd_pvdb = {}
     for ii, input_name in enumerate(sm.input_names):
         cmd_pvdb[input_name] = {"type": "float", "prec": 8, "value": sm.input_ranges[ii][0]}
@@ -24,29 +31,23 @@ def launch_ca_server():
     for ii, output_name in enumerate(sm.output_names):
         sim_pvdb[output_name] = {"type": "float", "prec": 8, "value": 0}
 
-    # Add in noise for fun
-    sim_pvdb["x_95coremit"]["scan"] = 0.2
-    noise_params = {"x_95coremit": {"sigma": 0.5e-7, "dist": "uniform"}}
-    # noise_params=None
 
-    server = SyncedSimPVServer("smvm", cmd_pvdb, sim_pvdb, noise_params, sm)
-    server.start_server()
+    if protocol == "ca":
+        from scalar_demo.ca_example.online_surrogate_model import SyncedSimPVServer
 
+        # Add in noise
+        sim_pvdb["x_95coremit"]["scan"] = 0.2
+        noise_params = {"x_95coremit": {"sigma": 0.5e-7, "dist": "uniform"}}
 
-@serve.command()
-def launch_pva_server():
-    from scalar_demo.model.surrogate_model import SurrogateModel
-    from scalar_demo.pva_example.pva_online_surrogate_model import SyncedSimPVServer
+        server = SyncedSimPVServer(cmd_pvdb, sim_pvdb, noise_params, sm)
+        server.start_server()
+            
 
-    sm = SurrogateModel(model_file=MODEL_FILE)
+    elif protocol == "pva":
+        from scalar_demo.pva_example.pva_online_surrogate_model import PVAServer
 
-    cmd_pvdb = {}
-    for ii, input_name in enumerate(sm.input_names):
-        cmd_pvdb[input_name] = {"type": "d", "value": sm.input_ranges[ii][0]}
+        server = PVAServer(cmd_pvdb, sim_pvdb, sm)
+        server.start_server()
 
-    sim_pvdb = {}
-    for ii, output_name in enumerate(sm.output_names):
-        sim_pvdb[output_name] = {"type": "d", "value": 0}
-
-    server = PVServer(cmd_pvdb)
-    server.start_server()
+    else:
+        print("Given protocol %s is not supported.", protocol)
